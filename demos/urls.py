462ad7503b68611000708d96f54f00be86777211
forklp <1637108208@qq.com>
2017-10-29 23:32:15 +0800

    k

--- demos/urls.py
+++ demos/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.demo1),
+    url(r'^demo1/',views.demo1)
 ]
--- demos/views.py
+++ demos/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 # Create your views here.
 def demo1(request):
-    return HttpResponse("duj")
+    return render(request, 'demos/demo1.html')
